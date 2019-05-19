ab89e039ca1ac53784ad9a4c35e2b67b60f7ed2f
unknown <Administrateur@acme.fr>
2019-05-19 15:15:55 +0200

    Ajout commentaires Script3.ps1

--- Projet05_LEGALL_Script03.ps1
+++ Projet05_LEGALL_Script03.ps1
@@ -2,14 +2,18 @@
 
 ## Marche à suivre
 
-# A l'execution du script, il est directement demandé à l'utilisateur de rentrer le nom de l'utilisateur.
-# Déclarer la variable $user pour permettre de trouver un utilisateur au sein de l'AD
+## Pour ce script silencieux, il est demandé à l'utilisateur de rentrer le nom de l'utilisateur faisant partie de plusieurs groupes.
+
+# Déclarer la variable $user pour permettre de trouver un utilisateur au sein d'un groupe de l'AD
 # Déclarer la variable $Value avec la commande foreach pour créer une boucle sur chaque élément de la variable $user
-# La commande est ensuite exécutée dans le bloc Try{} pour ajouter la liste dans le fichier texte
+# La commande est ensuite exécutée dans le bloc Try{} pour ajouter la liste des groupes dans un fichier texte
 # Si tout s'est bien déroulé, la commande envoie un echo
 # Le bloc Catch{}, renvoyer un message d'erreur si la première commande n'a pas fonctionnée
 # Enfin, un code d'erreur est indiqué par 0 ou 1 grâce au $LASTEXITCODE
-
+# Dans la fonction Param, il est possible de demander à l'utilisateur de saisir le chemin pour envoyer le résultat de la commande dans un fichier .txt
+	# Décommenter tout le bloc Param
+	# Rajouter une virgule après $user
+	# Commenter la variable $chemin dans la partie Variables chemins.
 
 Param(
 [Parameter(Mandatory=$True)]
@@ -18,12 +22,14 @@ Param(
 #[String]$chemin
 )
 
-#Variables
+#Variables chemins
 $fichier="C:\AD_Users\Projet05_LEGALL_AD03.txt"
 $file_Log = "C:\Scripts\Logs\Error_Script3_Log.txt"
 
+#Variable utilisateur
 $user = Get-ADUser -Filter {sAmaccountName -like $User} -SearchBase "DC=acme,DC=fr" -Properties MemberOf
 
+#Variable boucle 
 $Value = foreach($u in $user){
 write-host  "`nGroupe"
  $u.name
